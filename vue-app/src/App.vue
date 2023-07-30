b7f5446d10047eea8dd4dcdb6653e59af2317257
Mitya Kors <mitya@clarityo.com>
2023-07-31 00:05:22 +0300

    apps

---
+++ vue-app/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+</style>
